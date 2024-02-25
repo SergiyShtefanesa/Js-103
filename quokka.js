f3632d750faed2303e34b61eb2b23a07853b2054
Сергій <Mr.devid9328@gmail.com>
2024-02-25 10:33:17 +0200

    MIne

---
+++ quokka.js
@@ -0,0 +1,79 @@
+// function result(num1, num2) {
+//   const sum = num1 + num2;
+//   return sum;
+// }
+
+// const bmi = result(88.55, 90);
+// console.log(bmi);
+
+// function checkAccess(subType) {
+//  return subType === "pro" || subType == "vip";
+// }
+
+// console.log(checkAccess('pro'));
+// console.log(checkAccess('vip'));
+// console.log(checkAccess('free'));
+
+// function toggleModalVisibility(isVisible) {
+//   return isVisible === !true || isVisible === false;
+// }
+
+// console.log(toggleModalVisibility(false));
+// console.log(toggleModalVisibility(true));
+
+// const fullName = "Jacob Mercer";
+// const firstName = fullName.slice(0, 5);
+// const lastName = fullName.slice(8);
+
+// console.log(fullName); // "Jacob Mercer"
+// console.log(firstName); // "Jacob"
+// console.log(lastName); // "Mercer"
+
+// const message = "Welcome to Bahamas!";
+// console.log(message); // "Welcome to Bahamas!"
+// console.log(message.toUpperCase()); // "Welcome to Bahamas!"
+// console.log(message.toLowerCase()); // "welcome to bahamas!"
+
+// function normalizeInput(input, to) {
+
+// }
+
+// normalizeInput("This ISN'T SpaM", "lower")
+// normalizeInput("This ISN'T SpaM", "upper")
+// normalizeInput("Big SALE", "lower")
+// normalizeInput("Big SALE", "lower")
+// normalizeInput("Stay Awhile and Listen", "lower")
+// normalizeInput("Stay Awhile and Listen", "upper")
+
+// const stars = 4;
+// let prise;
+
+// switch (stars) {
+// 	case 1:
+// 		prise = 20;
+// 		break;
+// 	case 2:
+// 		prise = 40;
+// 		break;
+// 	case 3:
+// 		prise = 50;
+// 		break;
+// 	case 4:
+// 		prise = 60;
+// 		break;
+// 	default:
+// 		console.log("Def");
+// }
+
+// console.log(prise);
+
+// function normalizeInput(input, to) {
+//   if (to === "upper") {
+//     return input.toUpperCase();
+//   } else {
+//     return input.toLowerCase();
+//   }
+// }
+
+// console.log(normalizeInput("This ISN'T SpaM", "lower"));
+
